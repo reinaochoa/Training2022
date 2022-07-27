636ac8b3bfa0869ca6359dc6b491e61c4e9ae7ac
reinaochoa <reinaochoa.00@gmail.com>
2022-07-27 11:11:28 -0500

    updating respository

--- reina-app/src/main/java/com/assignment5/Employee.java
+++ reina-app/src/main/java/com/assignment5/Employee.java
@@ -1,7 +1,11 @@
 package com.assignment5;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Employee {
    int employeeID;
@@ -61,17 +65,33 @@ public class Employee {
 	   emp.add(new Employee(012, "Jane Doe", 30, "Female","HR", 2010, 75000));
 	   emp.add(new Employee(013, "Jackson Rich", 32, "Male","HR", 2011, 75000));
 	   emp.add(new Employee(014, "John Low", 40, "Male","SE", 2022, 100000));
-	   emp.add(new Employee(015, "Mark Jee", 21, "Female","SE", 2022, 60000));
+	   emp.add(new Employee(015, "Mark Jee", 21, "Female","SE", 2008, 60000));
 
 	   
  	   //i) How many male and female employees are there in this organization?
+	   Map<String, Long> cnt = emp.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+		System.out.println(cnt);
+	
 	  //ii) Find out Average age of male and female emps?
+		Map<String, Double> average = emp.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getAge)));
+		System.out.println(average);
 	 //iii) Find the highest paid employee in this org?
+		Optional<Employee> highestPaidEmp = emp.stream().collect(Collectors.maxBy(Comparator.comparingInt(Employee::getSalary)));
+		Employee highestEmp = highestPaidEmp.get();
+		System.out.println("Highest Paid Employee: "+highestEmp.employeeName+" with Salary: "+highestEmp.employeeSalary);
 	//iv) List all te names of employee who has joined after 2015?
+		emp.stream().filter(e -> e.getStartDate() > 2015).map(Employee::getName).forEach(System.out::println);
 		//v) Find out the senior most employee in this org?
+		Optional<Employee> seniorMostEmp = emp.stream().sorted(Comparator.comparingInt(Employee::getStartDate)).findFirst();
+		Employee senior = seniorMostEmp.get();
+		System.out.println("Senior Employee is: "+senior.employeeName);
 		// vi)Count the number of emps in each department?
-	//   vii)Find male and female employees are in Maintenance department?
+		Map<String, Long> DeptCnt = emp.stream().collect(Collectors.groupingBy(Employee::getDept, Collectors.counting()));
+		System.out.println(DeptCnt);
+		//   vii)Find male and female employees are in Maintenance department?
 //	   viii) Find the average slary of male and female employees?
+		Map<String, Double> avg = emp.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getSalary)));
+		System.out.println(avg);
 	   //ix) Differentiat eht the employees who are younger or equal to 30 yrs from those employees who older than 25 years
 	// x) List down the names of all employees in each department?
 
